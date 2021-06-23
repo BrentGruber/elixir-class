18a8b9a46f1f888fd614224628dca9b2fbbc9ebb
brent.gruber77@gmail.com <brent.gruber77@gmail.com>
2021-06-23 00:17:02 -0400

    getting started with structs

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,18 +1,30 @@
 defmodule Identicon do
   @moduledoc """
-  Documentation for `Identicon`.
+  Generate an identicon based on an input string
   """
+
+  def main(input) do
+    input
+    |> hash_string
+    |> pick_color
+  end
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello()
-      :world
+    generates an Image struct containing an MD5 hash list of hexadecimal numbers from a string `input`
 
   """
-  def hello do
-    :world
+  def hash_string(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+
+  @doc """
+    Creates an RGB color from the first 3 values of an image hex list
+
+  """
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
   end
 end
